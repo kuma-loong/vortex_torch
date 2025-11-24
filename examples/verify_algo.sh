81d7e587621806c6b4dcbf7b67c776d245f866fd
dreaming-panda <chenzhuoming911@gmail.com>
2025-11-23 22:50:55 -0500

    verify_algo

--- examples/verify_algo.sh
+++ examples/verify_algo.sh
@@ -10,7 +10,7 @@ sparse_algos=(
 for algo in "${sparse_algos[@]}"; do
   echo ">>> Running verify_algo.py with --vortex-module-name ${algo}"
   python examples/verify_algo.py \
-    --trials 2 \
+    --trials 8 \
     --topk-val 30 \
     --vortex-module-name "${algo}" \
     --model-name Qwen/Qwen3-1.7B
